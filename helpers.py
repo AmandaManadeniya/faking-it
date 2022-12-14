72bef2d79de5ae571130c33f03ed8e6b0710253a
AmandaManadeniya <87955258+AmandaManadeniya@users.noreply.github.com>
2022-12-14 23:01:01 +1100

    Added name and binding inputs
    Reworked programs to use KivyMD library and implemented:
    * Themes
    * Name input
    * Buttons
    * Binding (print to console)
    * Code sourced from https://github.com/attreyabhatt

---
+++ helpers.py
@@ -0,0 +1,9 @@
+username_input = """
+MDTextField:
+    hint_text: "Enter player name"
+    helper_text: "or click on generate random name"
+    helper_text_mode: "on_focus"
+    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x:None
+    width:300
+"""
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from tkinter import VERTICAL
-from kivy.app import App 
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button 
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+import helpers
+from kivy.lang import Builder
+from nltk.corpus import wordnet as wn
+import random
 
-class BoxLayoutExample(BoxLayout): 
-    pass 
-"""
-    def __init__(self, **kwargs): #**kwargs --> needed for kivy --> we can ignore 
-        super().__init__(**kwargs) 
-        self.orientation = "vertical"
-        b1 = Button(text = "Save name")
-        self.add_widget(b1)
-"""
 
-class MainWidget(Widget): 
-    pass 
+class DemoApp(MDApp):
 
-class LabApp(App): 
-    pass
+    def build(self):
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_hue = "500"
+        self.theme_cls.theme_style = "Dark"
+        screen = Screen()
 
-LabApp().run()
+        self.username = Builder.load_string(helpers.username_input)
+        btn_flat = MDRectangleFlatButton(text='Submit',
+                                         pos_hint={'center_x': 0.5, 'center_y': 0.4},
+                                         on_release=self.show_data)
+
+        name_btn = MDRectangleFlatButton(text='Generate random name',
+                                         pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                                         on_release=self.random_name)
+
+        screen.add_widget(btn_flat)
+        screen.add_widget(name_btn)
+        screen.add_widget(self.username)
+        return screen
+
+    def show_data(self,obj):
+        print(self.username.text)
+
+    def random_name(self,obj): 
+
+        rand_name = ["Popular Pete", "Mellow Mel", "Jiggly James", "Naughty Niko", "Astute AJ", "Sloppy Sharky", "Silly Sally" ]
+
+        print(random.choice(rand_name))
+
+DemoApp().run()
